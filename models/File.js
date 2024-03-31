748b610cadc715b09d6c572fa88d82cb54d4a071
sobhan2 <sobhanhero2001@gmail.com>
2024-03-31 09:42:46 +0530

    1

---
+++ models/File.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const File = new  mongoose.Schema({
+    path: {
+        type: String,
+        required: true
+    },
+    originalName: {
+        type: String,
+        required: true
+    },
+    password: String,
+    downloadCount: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+})
+
+mongoose.model('File', File);
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-import express from  'express';
+require("dotenv").config();
+const multer = require("multer");
+const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const File = require( "./models/File.js" );
+var fs = require('fs') ; 
+
+const express = require( 'express');
 const app = express();
+
+const upload =  multer({ dest: "uploads" });
+
+mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
 
@@ -7,4 +18,19 @@ app.get( '/', ( req, res ) => {
     res.render( 'index' );
 })
 
-app.listen(3000)
+app.post("/upload", upload.single("file"), async (req, res) => {
+    const fileData = {
+
+        path: req.file.path,
+        originalName:  req.file.originalname,
+       
+    }
+    if  (req.body.password != null && req.body.password != "") {
+        fileData.password = await bcrypt.hashSync(req.body.password,10);
+    }
+    const file = await File.create(fileData);
+    console.log(file);
+    res.send(file.originalName)
+})
+
+app.listen( process.env.PORT)
